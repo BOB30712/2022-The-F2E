6e3b473c0d61d2e9e50f19552b3aa0e66ff286dd
Bobby <a0930023855@gmail.com>
2022-11-16 22:26:40 +0800

    更新

--- week2/week2/src/views/PdfPractice.vue
+++ week2/week2/src/views/PdfPractice.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-      <input type="file" accept="application/pdf" placeholder="選擇PDF檔案" />
+      <div id="test" @dragover.prevent="test2" style="width: 300px;height: 300px;background-color: aqua;">
+      </div>
+      <input ref="inputfile" type="file" accept="application/pdf" placeholder="選擇PDF檔案" />
     </div>
     <div class="d-flex justify-content-center">
     <canvas id="canvas" style="border: 1px solid #000"> </canvas>
@@ -9,6 +11,10 @@
     <img class="sign" style="border: 1px solid #000" width="250" height="150" />
 
     <button class="download">download PDF</button>
+    <button id="forward" :disabled="page==1">上一頁</button>
+    <button id="next" :disabled="page==maxPage">下一頁</button>
+    <p>第{{page}}頁/共{{maxPage}}頁</p>
+    <p>當前檔案大小: {{filesize}} MB</p>
 </template>
 
 <style lang="scss">
@@ -22,7 +28,18 @@
 /* eslint-disable */
 import { fabric } from 'fabric'
 export default{
+    data () {
+        return {
+            page:1,
+            maxPage:0,
+            filesize:0,
+            testData:{},
+        }
+    },
     methods: {
+        test2(){
+            console.log("成功")
+        },
         readBlob (blob) {
             return new Promise((resolve, reject) => {
                 const reader = new FileReader();
@@ -31,15 +48,14 @@ export default{
                 reader.readAsDataURL(blob);
             });
         },
-        async printPDF (pdfData) {
+        async printPDF (pdfData,page) {
             const Base64Prefix = "data:application/pdf;base64,";
             pdfData = await this.readBlob(pdfData);
             const data = atob(pdfData.substring(Base64Prefix.length));
-
             // Using DocumentInitParameters object to load binary data.
             const pdfDoc = await pdfjsLib.getDocument({ data }).promise;
-            const pdfPage = await pdfDoc.getPage(1);
-
+            const pdfPage = await pdfDoc.getPage(page);
+            this.maxPage = pdfDoc._pdfInfo.numPages 
             const viewport = pdfPage.getViewport({ scale: 2 });
             // const viewport = pdfPage.getViewport({ scale: window.devicePixelRatio });
             const canvas = document.createElement("canvas");
@@ -66,24 +82,76 @@ export default{
         }
     },
     mounted () {
-        const canvas = new fabric.Canvas("canvas");
+        /*
+        1KB = 1,024 bytes
+        1MB = 1,024 KB = 1,048,576 bytes
+        1GB = 1,024 MB = 1,048,576 KB = 1,073,741,824 bytes
+        */
+        let canvas = new fabric.Canvas("canvas");
         document.querySelector("input").addEventListener("change", async (e) => {
-        canvas.requestRenderAll();
-        const pdfData = await this.printPDF(e.target.files[0]);
-        const pdfImage = await this.pdfToImage(pdfData);
-
-        // 調整canvas大小
-        canvas.setWidth(pdfImage.width / window.devicePixelRatio);
-        canvas.setHeight(pdfImage.height / window.devicePixelRatio);
-        canvas.setBackgroundImage(pdfImage, canvas.renderAll.bind(canvas));
+            canvas.requestRenderAll();
+            const pdfData = await this.printPDF(e.target.files[0],1);
+            this.testData = e.target.files[0]
+            this.filesize = (e.target.files[0].size/1048576).toFixed(2)
+            console.log(e.target.files[0])
+            this.page=1
+            const pdfImage = await this.pdfToImage(pdfData);
+            // 調整canvas大小
+            canvas.setWidth(pdfImage.width / window.devicePixelRatio);
+            canvas.setHeight(pdfImage.height / window.devicePixelRatio);
+            canvas.setBackgroundImage(pdfImage, canvas.renderAll.bind(canvas));
         });
+        document.querySelector("#test").addEventListener("drop", async (e) => {
+            e.preventDefault();
+            canvas.requestRenderAll();
+            const pdfData = await this.printPDF(e.dataTransfer.files[0],1);
+            this.testData = e.dataTransfer.files[0]
+            this.filesize = e.target.files[0].size/1048576
+            this.page=1
+            const pdfImage = await this.pdfToImage(pdfData);
+            // 調整canvas大小
+            canvas.setWidth(pdfImage.width / window.devicePixelRatio);
+            canvas.setHeight(pdfImage.height / window.devicePixelRatio);
+            canvas.setBackgroundImage(pdfImage, canvas.renderAll.bind(canvas));
+        });
+
+        document.querySelector("#next").addEventListener("click", async (e) => {
+            e.preventDefault();
+            canvas.requestRenderAll();
+            if(this.page!=this.maxPage){
+                this.page=this.page+1;
+            }
+            const pdfData = await this.printPDF(this.testData,this.page);
+            const pdfImage = await this.pdfToImage(pdfData);
+            // 調整canvas大小
+            canvas.setWidth(pdfImage.width / window.devicePixelRatio);
+            canvas.setHeight(pdfImage.height / window.devicePixelRatio);
+            canvas.setBackgroundImage(pdfImage, canvas.renderAll.bind(canvas));
+        });
+
+        document.querySelector("#forward").addEventListener("click", async (e) => {
+            e.preventDefault();
+            canvas.requestRenderAll();
+            if(this.page!=1){
+                this.page=this.page-1;
+            }
+            const pdfData = await this.printPDF(this.testData,this.page);
+            const pdfImage = await this.pdfToImage(pdfData);
+            // 調整canvas大小
+            canvas.setWidth(pdfImage.width / window.devicePixelRatio);
+            canvas.setHeight(pdfImage.height / window.devicePixelRatio);
+            canvas.setBackgroundImage(pdfImage, canvas.renderAll.bind(canvas));
+        });
+
+        
 
         // 加入簽名
         const sign = document.querySelector(".sign");
         if (localStorage.getItem("img")) {
         sign.src = localStorage.getItem("img");
+        this.testImg =  localStorage.getItem("img");
         }
-
+        
         sign.addEventListener("click", () => {
         if (!sign.src) return;
         fabric.Image.fromURL(sign.src, function (image) {
@@ -93,6 +161,7 @@ export default{
             canvas.add(image);
         });
         });
+        
 
         // 下讚PDF
         const pdf = new jsPDF();
